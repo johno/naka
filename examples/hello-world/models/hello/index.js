10ebe07df47169b34fdffca9caabb1df969f9bf1
John Otander <johnotander@gmail.com>
2016-06-23 18:24:16 -0600

    Tweak style of hello model

--- examples/hello-world/models/hello/index.js
+++ examples/hello-world/models/hello/index.js
@@ -17,7 +17,16 @@ module.exports = {
   },
 
   actions: {
-    decrement: (action, state, dispatch) => dispatch('hello.reducers.setCount', { count: state.hello.count - 1 }),
-    increment: (action, state, dispatch) => dispatch('hello.reducers.setCount', { count: state.hello.count + 1 })
+    decrement: (action, state, dispatch) => {
+      dispatch('hello.reducers.setCount', {
+        count: state.hello.count - 1
+      })
+    },
+
+    increment: (action, state, dispatch) => {
+      dispatch('hello.reducers.setCount', {
+        count: state.hello.count + 1
+      })
+    }
   }
 }
